a944b004d2120c2d264eea7c465ce61faba58190
Sarah Hall <swatson1994@yahoo.com>
2022-02-24 21:59:26 -0500

    Reassign string variable declarations

--- variables.rb
+++ variables.rb
@@ -1,12 +1,14 @@
-#String declarations
+#String declarations based on meals
 breakfast = "Eggs and toast"
 lunch = "Literally just some hard salami"
 dinner = "Leftover chicken and rice"
 
+#Below are variables assigned to integers based off a dice roll for character creation
 str_roll = 20
 con_roll = 8
 dex_roll = 15
 
+# Below are variables assigned to floats, based off the characters resistances/weaknesses
 ice_resist = 0.75
 pois_resist = 0.5
 fire_weak = 2.0
@@ -14,3 +16,8 @@ fire_weak = 2.0
 shield_equip = false
 armor_equip = true
 stealth_dis = true
+
+# Below we've reassigned strings based on meals, as if it were a different day
+breakfast = "Nothing, slept in"
+lunch = "Noodles and veggies"
+dinner = "Spaghetti squash and mushrooms"
